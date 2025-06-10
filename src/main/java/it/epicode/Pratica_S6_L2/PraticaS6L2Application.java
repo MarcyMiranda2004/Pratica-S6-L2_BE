992782231ec5d6e17f6651c446d92e96b47d5db9
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-06-10 15:53:57 +0200

    Initial commit

---
+++ src/main/java/it/epicode/Pratica_S6_L2/PraticaS6L2Application.java
@@ -0,0 +1,13 @@
+package it.epicode.Pratica_S6_L2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaS6L2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaS6L2Application.class, args);
+	}
+
+}
